5033e0440fdadbefd8b6a0c6ff5a266c9fcb34a8
yuna sung <yuna.sung.1003@gmail.com>
2021-09-11 17:18:08 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div className="App">
+    <h1>CRUD APPLICATION</h1>
+
+    <div className="form">
+      <label>Movie Name:</label>
+      <input type="text" name="movieName"/>
+      <label>Review:</label>
+      <input type="text" name="review"/>
+
+      <button>Submit</button>
     </div>
-  );
+
+  </div>;
 }
 
 export default App;
